1397411bc6303f8695deba394b8cbb86a0bf8780
Alexey Kuznetsov <axet@me.com>
2017-05-20 15:06:24 +0300

    fix locale

--- app/src/main/java/com/github/axet/hourlyreminder/services/AlarmService.java
+++ app/src/main/java/com/github/axet/hourlyreminder/services/AlarmService.java
@@ -4,9 +4,11 @@ import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Handler;
 import android.os.IBinder;
@@ -83,7 +85,7 @@ public class AlarmService extends Service implements SharedPreferences.OnSharedP
     List<ReminderSet> reminders;
     PowerManager.WakeLock wl;
     PowerManager.WakeLock wlCpu;
-    Handler handler;
+    Handler handler = new Handler();
     Runnable wakeClose = new Runnable() {
         @Override
         public void run() {
@@ -107,7 +109,6 @@ public class AlarmService extends Service implements SharedPreferences.OnSharedP
             public void check() {
             }
         };
-        handler = new Handler();
         sound = new Sound(this);
         alarms = HourlyApplication.loadAlarms(this);
         reminders = HourlyApplication.loadReminders(this);
